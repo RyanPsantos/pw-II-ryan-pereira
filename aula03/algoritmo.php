c1aff32f576968a198491849c84cc1044e128aae
Ryan Pereira <Ryanpsantos124@gmail.com>
2024-03-17 20:13:27 -0300

    2

--- aula03/algoritmo.php
+++ aula03/algoritmo.php
@@ -1,85 +1,18 @@
 
-<h1>Par Impar:</h1>
+<h1>Par ou Ímpar:</h1>
 
 <?php
 
-$comparacao;
-$a = 34;
-$b = 42;
-$c = 9;
-$d = 17;
-$e = 56;
-$f = 55;
+$valor;
 
-while($a) break; {
+for ($valor = 25; $valor <= 60; $valor++) {
     
-    if($a % 2 == 0){
-    echo $a, " é par. <br>";
+    if($valor % 2 == 0){
+    echo $valor, " é par. <br>";
 }
-
     else{
-    echo $a, " é impar. <br>";
+    echo $valor, " é impar. <br>";
 }
-}
-
-
-while($b) break; {
-
-    if($b % 2 == 0){
-    echo $b, " é par. <br>";
-}
-
-    else{
-    echo $b, " é impar. <br>";
-}
-}
-
-
-while($c) break; {
-
-    if($c % 2 == 0){
-    echo $c, " é par. <br>";
-}
-
-    else{
-    echo $c, " é impar. <br>";
-}  
-}
-
-
-while($d) break; {
-
-    if($d % 2 == 0){
-    echo $d, " é par. <br>";
-}
-
-    else{
-    echo $d, " é impar. <br>";
-}
-}
-
-
-while($e)break;{
-
-    if($e % 2 == 0){
-    echo $e, " é par. <br>";
-}
-
-    else{
-    echo $e, " é impar. <br>";
-}
-}
-
-
-while($f)break;{
-
-    if($f % 2 == 0){
-    echo $f, " é par. <br>";
-}
-
-    else{
-    echo $f, " é impar. <br>";
-}   
 }
 
  
